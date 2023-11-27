662a399bed056326790f8e743cd97274fd3148be
JS Kim <kiqbbq0602@gmail.com>
2023-11-27 19:25:05 +0900

    Email 오류 수정

---
+++ gun/src/main/java/com/ict/gun/common/email/dto/EmailPostDto.java
@@ -0,0 +1,8 @@
+package com.ict.gun.common.email.dto;
+
+import lombok.Getter;
+
+@Getter
+public class EmailPostDto {
+    private String email;
+}
---
+++ gun/src/main/java/com/ict/gun/common/email/dto/EmailResponseDto.java
@@ -0,0 +1,10 @@
+package com.ict.gun.common.email.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class EmailResponseDto {
+    private String code;
+}
