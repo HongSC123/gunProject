252029c912d676dcffc2c3d79791113c7dff7d58
HongSC <hsc119119@naver.com>
2023-12-04 10:01:23 +0900

    1204

---
+++ src/main/java/com/ict/gun/ref/controller/RefController.java
@@ -0,0 +1,13 @@
+package com.ict.gun.ref.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RequiredArgsConstructor
+@CrossOrigin
+@RestController
+public class RefController {
+}
